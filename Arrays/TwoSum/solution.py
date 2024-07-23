9b4c77f2583249526979affa81d7be827c6dd4da
Samantha Zammit <samanthazammit98@hotmail.com>
2024-07-22 23:09:08 -0400

    update var

--- Arrays/TwoSum/solution.py
+++ Arrays/TwoSum/solution.py
@@ -8,16 +8,16 @@ class Solution(object):
         # Store the keys we have already viewed
         seen_keys = {}
         # Iterate through the list of nums to see their key and value pairs
-        for key, num in enumerate(nums):
-            print(key, num)
+        for i, num in enumerate(nums):
+            print(i, num)
             # Calculate the complement to the current num value needed to equal target
             complement = target - num
             print(complement)
             # The first iteration will not see anything in seen_keys but that is okay as we beging actually comparing on key #1 (as we begin at key #0)
             if complement in seen_keys:
                 # If we find the complement in the seen_keys we will find it in the complements position, as we are storing the index in the position of the num value
-                return [seen_keys[complement], key]
+                return [seen_keys[complement], i]
             # store the index in the position of the num value
-            seen_keys[num] = key
+            seen_keys[num] = i
             print(seen_keys)
         return [0,0]
